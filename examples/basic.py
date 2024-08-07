0dc8e45cfbb5352a00964a6599531e19932c500c
Ivo Bellin Salarin <me@nilleb.com>
2024-08-07 12:49:28 +0200

    fix: sample

--- examples/basic.py
+++ examples/basic.py
@@ -7,30 +7,32 @@ basic usages
 
 """
 
+
 def my_event_callback(event_data):
-    ''' 
-    simple event callback to listen to incoming event/messages. 
+    """
+    simple event callback to listen to incoming event/messages.
     whenever this function is called, it will retrieve the current incoming event or messages.
-    '''
+    """
     print("Received event data:", event_data)
 
-def listening_message(minutes):
-    """ Stream messages for 'minutes' duration"""
-    end_time = time.time() + minutes * 60  
-    while time.time() < end_time:
-        my_event_callback()
-  
-if __name__ == "__main__":
 
-    phone = "6283139750000" # make sure to attach country code + phone number
+def listening_message(minutes):
+    """Stream messages for 'minutes' duration"""
+    end_time = time.time() + minutes * 60
+    while time.time() < end_time:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    phone = "6283139750000"  # make sure to attach country code + phone number
     message = "Hello World!"
 
-    whatsapp = WhatsApp(event_callback=my_event_callback)
+    whatsapp = WhatsApp(on_event=my_event_callback)
 
     whatsapp.connect()
 
     message_sent = whatsapp.sendMessage(phone=phone, message=message)
-    
+
     listening_message(5)
 
     whatsapp.disconnect()
